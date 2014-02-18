92f90fd1511a1a64fee952d730596a80911a5add
posim <posim@ip-172-5.main.oberlin.edu>
2014-02-18 14:04:58 -0500

    Added crazy camera rotation script

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour {
+
+	public Rigidbody2D playerBody;
+	public float rotateHorizontal = 0.01f;
+	public float rotateVertical = 0.1f;
+	public float resetTime = 0.1f;
+
+	
+
+	void Update ()
+	{
+		transform.Rotate (Mathf.Abs (playerBody.velocity.normalized.y * rotateVertical),0f,Mathf.Abs (playerBody.velocity.normalized.x * rotateHorizontal));
+		transform.rotation = Quaternion.Lerp (Quaternion.identity,transform.rotation,resetTime);
+	}
+}
