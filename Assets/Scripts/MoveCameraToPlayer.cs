65c23a0ac7fc8e2e4db6cd9a508f435d8e7c5f77
posim <posim@ip-172-5.main.oberlin.edu>
2014-02-18 16:11:19 -0500

    Added player sprite. Everything is janky looking

--- Assets/Scripts/MoveCameraToPlayer.cs
+++ Assets/Scripts/MoveCameraToPlayer.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class MoveCameraToPlayer : MonoBehaviour {
 
 	public float zoomSpeed; //How fast the camera should zoom in and out on the player
+	public float trackXSpeed = 1; //Should we track the horizontal
+	public float trackYSpeed = 1; //Should we track the vertical
 
 	public GameObject player;
 	
 	void Update () {
-		this.transform.position = new Vector3(player.transform.position.x,player.transform.position.y,transform.position.z);
+		this.transform.position = new Vector3(player.transform.position.x * trackXSpeed,player.transform.position.y * trackYSpeed,transform.position.z);
 		camera.orthographicSize = Mathf.Lerp (camera.orthographicSize,player.rigidbody2D.velocity.magnitude,zoomSpeed);
 	}
 }
