11de201fe9b5ab1f038a2611d0dbba46d5dc4850
Paul Buser <pbuser@oberlin.edu>
2014-03-07 15:19:56 -0500

    Recording and playback of movement KINDA works. Stutters a lot. Gotta work on that.

--- Assets/Scripts/PlayerRecordAndPlayback.cs
+++ Assets/Scripts/PlayerRecordAndPlayback.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerRecordAndPlayback : MonoBehaviour {
 
 	public Transform playerTransform;
-	private Queue recordedFrames;
-	private float startTime;
-
+	private Queue<RecordedFrame> recordingQueue;
+	private float timeSinceLastFrame;
+	private RecordingState recordingState;
 	enum RecordingState {Idle, Record, Play};
-	public class CapturedFrame
+	class RecordedFrame
 	{
-		float timeSinceLast;
-		Vector3 playerPosition;
-		CapturedFrame(float timeSinceLast, Vector3 playerPosition)
+		public float timeSinceLast;
+		public Vector3 playerPosition;
+
+		/// <summary>
+		/// Initializes a new recorded frame which includes a delta time and x,y,z position.
+		/// </summary>
+		/// <param name="timeSinceLast">Time since last frame.</param>
+		/// <param name="playerPosition">Player position.</param>
+		public RecordedFrame(float timeSinceLast, Vector3 playerPosition)
 		{
 			this.timeSinceLast = timeSinceLast;
 			this.playerPosition = playerPosition;
@@ -22,30 +28,64 @@ public class PlayerRecordAndPlayback : MonoBehaviour {
 
 	void Awake()
 	{
-		this.recordedFrames = new Queue<CapturedFrame>();
+		this.recordingQueue = new Queue<RecordedFrame>();
+		recordingState = RecordingState.Idle;
+	}
+
+	void SwitchState()
+	{
+		if (this.recordingState == RecordingState.Idle)
+		{
+			Debug.Log ("STATE: Record");
+			this.recordingState = RecordingState.Record;
+			// Clear the recorded frames
+			recordingQueue = new Queue<RecordedFrame>();
+		}
+		else if (this.recordingState == RecordingState.Record)
+		{
+			Debug.Log ("STATE: Play");
+			this.recordingState = RecordingState.Play;
+		}
+		else if (this.recordingState == RecordingState.Play)
+		{
+			Debug.Log ("STATE: Idle");
+			this.recordingState = RecordingState.Idle;
+		}
 	}
 
 	void Update()
 	{
-		/* if we press the rec button
-		 * 	if we're idle
-		 * 	 start recording
-		 *  if we're recording
-		 *   start playing
-		 *  if we're playing
-		 *   stop playing
-		 */
+		if (Input.GetKeyDown (KeyCode.R))
+		{
+			SwitchState ();
+		}
+
+		if (this.recordingState == RecordingState.Play)
+		{
+			if (recordingQueue.Count != 0)
+			{
+				RecordedFrame nextFrame = recordingQueue.Peek();
+				timeSinceLastFrame += Time.deltaTime;
+				if (nextFrame.timeSinceLast < timeSinceLastFrame)
+				{
+					playerTransform.position = nextFrame.playerPosition;
+					timeSinceLastFrame = 0f;
+					recordingQueue.Dequeue();
+				}
+			}
+			else
+			{
+				recordingState = RecordingState.Idle;
+			}
+		}
+		else if (this.recordingState == RecordingState.Record)
+		{
+			RecordedFrame thisFrame = new RecordedFrame(Time.deltaTime, playerTransform.position);
+			recordingQueue.Enqueue(thisFrame);
+		}
+
 
 	}
 
-	void CaptureFrame()
-	{
-		CapturedFrame newFrame = new CapturedFrame(Time.deltaTime,playerTransform.position);
-		this.recordedFrames.Enqueue(newFrame);
-	}
-
-	void PlayBack()
-	{
-
-	}
+	
 }
